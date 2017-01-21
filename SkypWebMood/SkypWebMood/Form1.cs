903e1f273dbdfc569909194f7aed2367f047d4d1
Tomáš Patera <tomas.patera@pnona.cz>
2017-01-21 16:44:09 +0100

    Create browser window

--- SkypWebMood/SkypWebMood/Form1.cs
+++ SkypWebMood/SkypWebMood/Form1.cs
@@ -12,9 +12,16 @@ namespace SkypWebMood
 {
     public partial class Form1 : Form
     {
+        browser browserwindow;
         public Form1()
         {
             InitializeComponent();
+            browserwindow = new browser();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browserwindow.Show();
         }
     }
 }
---
+++ SkypWebMood/SkypWebMood/browser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace SkypWebMood
+{
+    
+    public partial class browser : Form
+    {
+        public browser()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+
+        public ChromiumWebBrowser prohlizec;
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            prohlizec = new ChromiumWebBrowser("www.google.com");
+            this.Controls.Add(prohlizec);
+            prohlizec.Dock = DockStyle.Fill;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            prohlizec.Load(textBox1.Text);
+        }
+    }
+}
